a8b543f0155dc79fe542578c7469715972c1613a
shotaro-ooe <shoooe97@gmail.com>
2018-11-09 17:23:30 +0900

    完成

---
+++ IntroClass/Assets/RobotMotion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotMotion {
+
+    Action<RobotScript, float> animation;
+    float duration;
+    float pastTime = 0;
+
+    public RobotMotion(RobotMotion src) : this(src.animation,src.duration)
+    {
+    }
+    public RobotMotion(Action<RobotScript, float> animation, float duration)
+    {
+        this.animation = animation;
+        this.duration = duration;
+    }
+    public bool Animate(RobotScript robot, float deltaTime)
+    {
+        pastTime += deltaTime;
+        animation(robot, pastTime / duration);
+        return pastTime >= duration;
+    }
+}
